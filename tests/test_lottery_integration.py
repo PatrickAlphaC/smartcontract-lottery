4d8f212a74a3ce268346c7919ef74d259ec0cec2
neotod <neotod@protonmail.com>
2022-03-24 13:37:59 +0430

    adding pytests fixtures for making the code cleaner adding .vscode dir to .gitignore

--- tests/test_lottery_integration.py
+++ tests/test_lottery_integration.py
@@ -1,24 +1,19 @@
-from brownie import network
-import pytest
 from scripts.helpful_scripts import (
-    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
     get_account,
     fund_with_link,
 )
-from scripts.deploy_lottery import deploy_lottery
 import time
 
 
-def test_can_pick_winner():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-        pytest.skip()
-    lottery = deploy_lottery()
+def test_can_pick_winner(check_local_blockchain_envs, lottary):
+    # Arrange (by fixtures)
+
     account = get_account()
-    lottery.startLottery({"from": account})
-    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
-    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
-    fund_with_link(lottery)
-    lottery.endLottery({"from": account})
+    lottary.startLottery({"from": account})
+    lottary.enter({"from": account, "value": lottary.getEntranceFee()})
+    lottary.enter({"from": account, "value": lottary.getEntranceFee()})
+    fund_with_link(lottary)
+    lottary.endLottery({"from": account})
     time.sleep(180)
-    assert lottery.recentWinner() == account
-    assert lottery.balance() == 0
+    assert lottary.recentWinner() == account
+    assert lottary.balance() == 0
